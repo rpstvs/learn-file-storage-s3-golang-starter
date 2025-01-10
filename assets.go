5305b60f2964aa995211760ddfa65535f60a7712
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-01-10 19:07:00 +0000

    added new implementation for file storage

--- assets.go
+++ assets.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +14,25 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
+	return filepath.Join(cfg.assetsRoot, assetPath)
+}
+
+func (cfg apiConfig) getAssetUrl(assetpath string) string {
+	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetpath)
+}
+
+func getAssetPath(videoID uuid.UUID, mediaType string) string {
+	ext := mediatypeToExt(mediaType)
+	return fmt.Sprintf("%s%s", videoID, &ext)
+}
+
+func mediatypeToExt(mediaType string) string {
+	parts := strings.Split(mediaType, "/")
+	if len(parts) != 2 {
+		return ".bin"
+	}
+	return "." + parts[1]
 }
